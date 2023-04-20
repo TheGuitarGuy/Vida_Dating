64b2b3265a7829d427bd3554f49bf2600b60e558
Kennion Gubler <gublerkennion@gmail.com>
2023-04-19 17:10:19 -0700

    Initial Commit

---
+++ Vida Dating/Vida_DatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vida_DatingApp.swift
+//  Vida Dating
+//
+//  Created by Kennion Gubler on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Vida_DatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
